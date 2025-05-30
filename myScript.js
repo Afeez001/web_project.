30dea1dc21e085c3a49867fb12fae98ce4ad0ae3
unknown <isholaadebola001gmail.com>
2025-05-30 13:48:35 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,17 @@
+
+console.warn("this is a warning");
+
+// function myFunction(){
+// 	document.getElementById("Demo").innerHTML = "Welcome Once again";
+// }
+
+console.log('Hello World')
+
+let x, y, z;
+x = 45;
+y = 23;
+z = x * y;
+
+function myFunction(){
+	document.getElementById("Demo2").innerHTML = z;
+}
---
+++ texting.js
@@ -0,0 +1,12 @@
+// thi is a object;
+const car = {};
+
+//this is the object properties
+car.type = "Roll Royces";
+car.color = "blue and white";
+car.weight = "500 lbs";
+car.version = 2009;
+
+//this is the commnand calling out the object
+document.getElementById("Demo").innerHTML = "the type of car is " + car.type;
+
